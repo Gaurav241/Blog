12497a9cca0eb4493cf84dfcff10298ecb222966
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-03-21 13:45:04 +0530

    Blog: Rewrite scroll-up logic code.

--- src/components/scroll-up-button.tsx
+++ src/components/scroll-up-button.tsx
@@ -1,21 +1,40 @@
 'use client';
 
+import { useCallback, useEffect, useState } from 'react';
 import { IoMdArrowRoundUp } from 'react-icons/io';
 
-import useScroll from '@/hooks/use-scroll';
-import { isTouchDevice } from '@/utils/is-touch-device';
-
 export default function ScrollUpButton() {
-  const { show, buttonRef } = useScroll();
+  const [show, setShow] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleScroll = useCallback(() => {
+    if (!show && window.scrollY > 500) setShow(true);
+    if (show && window.scrollY <= 500) setShow(false);
+  }, [show]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
 
   return (
-    <button
-      ref={buttonRef}
-      className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-red-500 text-2xl text-white opacity-70 transition-all duration-300 md:bottom-20 md:right-20 md:h-14 md:w-14 md:text-3xl ${
-        !show && 'hidden'
-      } ${!isTouchDevice() && 'hover:scale-110 hover:opacity-100'}`}
-    >
-      <IoMdArrowRoundUp />
-    </button>
+    <>
+      {show && (
+        <div className="fixed bottom-4 right-4 z-20" style={{ opacity: 1 }}>
+          <button
+            onClick={scrollToTop}
+            className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-red-500 text-2xl text-white opacity-70 transition-all duration-300 md:bottom-20 md:right-20 md:h-14 md:w-14 md:text-3xl"
+          >
+            <IoMdArrowRoundUp />
+          </button>
+        </div>
+      )}
+    </>
   );
 }
